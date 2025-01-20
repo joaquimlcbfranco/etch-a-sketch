1d4a02a9c34bf1b7dabfd7a2aa894f363866f109
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-01-20 11:07:35 +0000

    Nest for loops to create row and squares for each one

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const MAX_WIDTH = 16;
 
 // Get container that will have the grid in it
 const grid = document.querySelector('#grid');
+let row = document.createElement('div');
 
 // Add childs to container with x height/width, taken from user's prompt input
-for (let i = 1; i < (MAX_HEIGHT * MAX_WIDTH); i++) {
-    const square = document.createElement('div');
-    grid.appendChild(square);
-    square.classList.add('grid-square');
+for (let i = 1; i <= MAX_HEIGHT; i++) {
+    row = document.createElement('div');
+    grid.appendChild(row);
+    row.classList.add('row');
+    for (let j = 1; j <= MAX_WIDTH; j++) {
+        const square = document.createElement('div');
+        row.appendChild(square);
+        square.classList.add('square');
+    }
 }
