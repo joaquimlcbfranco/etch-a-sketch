df53b2e4ca2cd21fb70818b627ae6caa289c7125
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-01-20 10:02:04 +0000

    Change height and width variable names, start i on 1

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Start variable with fixed number for height/width
-const HEIGHT, const WIDTH = 16;
+const MAX_HEIGHT = 16;
+const MAX_WIDTH = 16;
 
 // Get container that will have the grid in it
 const grid = document.querySelector('#grid');
+console.log(grid);
 
 // Add childs to container with x height/width, taken from user's prompt input
-for (let i = 0; i < HEIGHT * WIDTH; i++) {
+for (let i = 1; i <= (MAX_HEIGHT * MAX_WIDTH); i++) {
     const square = document.createElement('div');
     grid.appendChild(square);
 }
