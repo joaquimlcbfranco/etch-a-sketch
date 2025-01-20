a8417effc9481e370c409cddb375772f0c268355
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-01-20 15:43:49 +0000

    Add function that displays warning
    Added a new function that displays a warning whenever the user inputs invalid grid size values:
        - Null
        - Lower than 0
        - Bigger than 100

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ let GRID_SIZE = document.querySelector('#grid-size').value;
 const grid = document.querySelector('.container');
 let row = document.createElement('div');
 
+// Call function to display grid when website is opened
+createGrid(GRID_SIZE, GRID_SIZE);
+paintSquares();
+
 // Function that add childs to container with x height/width, taken from user's inputs. Removes all the previous childs before adding
 function createGrid(height, width) {
     for (let i = 1; i <= height; i++) {
@@ -19,23 +23,46 @@ function createGrid(height, width) {
     }
 }
 
-// Call function to display grid when website is opened
-createGrid(GRID_SIZE, GRID_SIZE);
-
 // Get measurement's apply button
 const measurementsButton = document.querySelector('.measurements-btn');
 
 // Listen for button click, when clicked call function with height and width values
+// Also restore the value for the squares variable after creating the grid
 measurementsButton.addEventListener('click', () => {
     GRID_SIZE = document.querySelector('#grid-size').value;
-    grid.innerHTML = '';
-    createGrid(GRID_SIZE, GRID_SIZE); 
+    if (GRID_SIZE > 100 || GRID_SIZE < 0 || !(GRID_SIZE)) {
+        displayWarning();
+    }
+    else {
+        grid.innerHTML = '';
+        warning.classList.add('hide')
+        createGrid(GRID_SIZE, GRID_SIZE);
+        paintSquares();
+    }
+    
 });
 
+// Initialize body variable. Stores the body div, so we can append child elements to it
+// Create a paragraph
+const body = document.querySelector('.body');
+let warning = document.createElement('p');
+warning.classList.add('warning');
+
+// Function that displays a warning when called. 
+function displayWarning() {
+    warning.textContent = 'Value must be an integer between 1 and 100!'
+    warning.classList.remove('hide')
+    body.appendChild(warning);
+}
+
 // Detects mouse movement over squares, changes square's background color
-squares = document.querySelectorAll('.square');
-squares.forEach((square) => {
-    square.addEventListener('mouseenter', () => {
-        square.style.backgroundColor = '#fd75fd'
-    });  
-});
+function paintSquares() {
+    squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.addEventListener('mouseenter', () => {
+            square.style.backgroundColor = '#fd75fd'
+        });  
+    });
+}
+
+    
