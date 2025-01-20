d8034c044e9e95ba03a7acf886d61f3b412a2487
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-01-20 10:41:24 +0000

    Set class of square after creating

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const MAX_WIDTH = 16;
 
 // Get container that will have the grid in it
 const grid = document.querySelector('#grid');
-console.log(grid);
 
 // Add childs to container with x height/width, taken from user's prompt input
-for (let i = 1; i <= (MAX_HEIGHT * MAX_WIDTH); i++) {
+for (let i = 1; i < (MAX_HEIGHT * MAX_WIDTH); i++) {
     const square = document.createElement('div');
     grid.appendChild(square);
+    square.classList.add('grid-square');
 }
