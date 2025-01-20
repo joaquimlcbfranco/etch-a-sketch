7fe3b223a8a59bfcbb9e2329f5fddebd24cd1538
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-01-20 15:23:29 +0000

    Change createGrid function, deletes squares before adding

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-// Start variable with fixed number for height/width
-const MAX_HEIGHT = 16;
-const MAX_WIDTH = 16;
+// Get default grid size value from the default input value
+let GRID_SIZE = document.querySelector('#grid-size').value;
 
-// Get container that will have the grid in it
-const grid = document.querySelector('#grid');
+// Get grid container and row div
+const grid = document.querySelector('.container');
 let row = document.createElement('div');
 
-// Add childs to container with x height/width, taken from user's prompt input
-for (let i = 1; i <= MAX_HEIGHT; i++) {
-    row = document.createElement('div');
-    grid.appendChild(row);
-    row.classList.add('row');
-    for (let j = 1; j <= MAX_WIDTH; j++) {
-        const square = document.createElement('div');
-        row.appendChild(square);
-        square.classList.add('square');
+// Function that add childs to container with x height/width, taken from user's inputs. Removes all the previous childs before adding
+function createGrid(height, width) {
+    for (let i = 1; i <= height; i++) {
+        row = document.createElement('div');
+        grid.appendChild(row);
+        row.classList.add('row');
+        for (let j = 1; j <= width; j++) {
+            const square = document.createElement('div');
+            row.appendChild(square);
+            square.classList.add('square');
+        }
     }
 }
 
-// Detect mouse movement over square, change square background color
+// Call function to display grid when website is opened
+createGrid(GRID_SIZE, GRID_SIZE);
+
+// Get measurement's apply button
+const measurementsButton = document.querySelector('.measurements-btn');
+
+// Listen for button click, when clicked call function with height and width values
+measurementsButton.addEventListener('click', () => {
+    GRID_SIZE = document.querySelector('#grid-size').value;
+    grid.innerHTML = '';
+    createGrid(GRID_SIZE, GRID_SIZE); 
+});
+
+// Detects mouse movement over squares, changes square's background color
 squares = document.querySelectorAll('.square');
 squares.forEach((square) => {
     square.addEventListener('mouseenter', () => {
